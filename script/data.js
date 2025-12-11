ee98c8a57103a0c974064e5ee9ab8168647c18ea
rtspark <robbytendean@gmail.com>
2025-12-11 04:59:43 +0000

    Final code

---
+++ script/data.js
@@ -0,0 +1,109 @@
+const JSON_BIN_BASE_URL = 'https://api.jsonbin.io/v3';
+const JSON_BIN_ID=""
+const JSON_BIN_MASTER_KEY=""
+
+function getLargestID(bucketList)
+{
+    let largestID = 0;
+    for (let b of bucketList)
+    {
+        if (parseInt(b.id) > largestID)
+        {
+            largestID = b.id;
+        }
+    }
+    return largestID;
+}
+
+async function loadData() {
+    try{        
+        const config = {
+            headers: {
+                "Content-Type":"application/json", //this line is not mandatory
+                'X-Master-Key': JSON_BIN_MASTER_KEY
+            }
+        };
+        const response = await axios.get(`${JSON_BIN_BASE_URL}/b/${JSON_BIN_ID}/latest`,config);
+        console.log("loadData.response: ",response);    
+        return response.data.record;    
+    }
+    catch(e)
+    {
+        console.log("Exception:",e);
+        return []; //empty array so for loop won't crash
+    }    
+};
+
+async function saveData(bucketList) {
+    try{        
+        const config = {
+            headers: {
+                "Content-Type":"application/json", //this line is not mandatory
+                'X-Master-Key': JSON_BIN_MASTER_KEY
+            }
+        };
+        //params:
+        //1. URL
+        //2. Array of Books
+        //3. config
+        const response = await axios.put(`${JSON_BIN_BASE_URL}/b/${JSON_BIN_ID}`,bucketList,config);
+        console.log("saveData.response: ",response);   
+        return response;    
+    }
+    catch(e)
+    {
+        console.log("Exception:",e);
+        return {
+            "error":e
+        }
+    }    
+}
+
+function addActivity(bucketList,country,city,activity,priority)
+{
+    let newID = getLargestID(bucketList) + 1; 
+    const newActivity = {
+        "id": newID,
+        "country": country,
+        "city": city,
+        "activity": activity,
+        "priority": priority,
+        "done": 0
+    }
+    bucketList.push(newActivity);
+    return saveData(bucketList);
+}
+
+function modifyActivity(bucketList,updateID,updateCountry,updateCity,updateActivity,updatePriority,updateDone)
+{
+    for(let b of bucketList)
+    {
+        if (b.id==updateID)
+        {
+            b.country=updateCountry;
+            b.city=updateCity;
+            b.activity=updateActivity;
+            b.priority=updatePriority;
+            b.done=updateDone;
+            break;
+        }
+    }
+    return saveData(bucketList);
+}
+
+function deleteActivity(bucketList,deleteID)
+{
+    let deleteIndex = -1;
+    let currentIndex = 0;
+    for(let b of bucketList)
+    {
+        if (b.id==deleteID)
+        {
+            deleteIndex=currentIndex;
+            break; 
+        }
+        currentIndex++;
+    }
+    bucketList.splice(deleteIndex,1);
+    return saveData(bucketList);
+}
---
+++ script/script.js
@@ -0,0 +1,114 @@
+document.addEventListener("DOMContentLoaded", async function() {
+    const bucketList = await loadData();
+    renderBucket(bucketList);
+    
+    let addTodoBtn = document.querySelector("#addActivity");
+    addTodoBtn.addEventListener("click", async function(){
+        //get country
+        let country =document.querySelector("#countryInput");
+        let countryValue = country.value;
+        //get city
+        let city =document.querySelector("#cityInput");
+        let cityValue = city.value;
+        //get activity
+        let activity =document.querySelector("#activityInput");
+        let activityValue = activity.value;
+        //get priority
+        let priority =document.querySelector("#priorityInput");
+        let priorityValue = priority.value;
+        //add todo
+        let response = await addActivity(bucketList,countryValue,cityValue,activityValue,priorityValue);
+        console.log("addTodoBtn.click:response.status: ",response["status"]);
+        console.log("addTodoBtn.click:response.error: ",response["error"]);
+        //check if success
+        if (response["status"]=="200" && !response["error"])
+        {
+            let saveStatus = document.querySelector("#saveStatus");
+            saveStatus.className="text-success";
+            saveStatus.innerText = "Activity is saved successfully"
+            //reset input
+            country.value="";
+            city.value="";
+            activity.value="";
+            priority.value="low";
+        }
+        else
+        {
+            //if error show message
+            let errorStatus = document.querySelector("#saveStatus");
+            errorStatus.className="text-danger";
+            errorStatus.innerText = "Error: " + response["error"];
+        }
+        //render todo list again
+        renderBucket(bucketList);        
+    });
+});
+
+//parameter is bucket
+function renderBucket(bucketList)
+{
+    let bucketContent = document.querySelector("#bucketContent");
+
+    //remove existing childs first
+    bucketContent.innerHTML="";
+
+    //generate child elements
+    for (let t of bucketList) {
+        let rowElement = document.createElement("div");
+        rowElement.className="row gy-3 pb-1 border-bottom border-dark text-start pb-3 pt-3";
+        rowElement.innerHTML=`
+            <div class="col-12 col-md-2">${t.country}</div>
+            <div class="col-12 col-md-2">${t.city}</div>
+            <div class="col-12 col-md-4">${t.activity}</div>
+            <div class="col-12 col-md-1"> 
+                <span class="badge ${t.priority == "high" ? 
+                    "bg-danger" : (t.priority == "medium" ? "bg-warning" : "bg-success")}">
+                    ${pascalCase(t.priority)}
+                </span> 
+            </div>
+            <div class="col-12 col-md-1">
+                <span class="badge ${t.done ? "bg-success" : "bg-warning"}">
+                    ${t.done ? "Done" : "Pending"}
+                </span>
+            </div>
+            <div class="col-12 col-md-2"> 
+                <button class="btn btn-primary btn-sm edit-button">Edit</button>
+                <button class="btn btn-danger btn-sm delete-button">Delete</button>
+            </div>
+        `;
+        
+        //add click event to edit button
+        let editBtn = rowElement.querySelector(".edit-button");
+        editBtn.addEventListener("click", function() {
+            let newCountry = prompt("Update Country: ", t.country);
+            let newCity = prompt("Update City: ", t.city);
+            let newActivity = prompt("Update Activity: ", t.activity);
+            let newPriority = prompt("Update Priority: ", t.priority);
+            let newDone = prompt("Update Status (Done/Pending):", t.done ? "Done" : "Pending").toLowerCase() == "done";
+            modifyActivity(bucketList,t.id,newCountry,newCity,newActivity,newPriority,newDone);
+            renderBucket(bucketList);
+        });
+
+        //add click event to delete button        
+        let deleteBtn = rowElement.querySelector(".delete-button");
+        deleteBtn.addEventListener("click", function() {
+            deleteActivity(bucketList,t.id);
+            renderBucket(bucketList);
+        });
+
+        bucketContent.appendChild(rowElement);
+    }   
+}
+
+function pascalCase(inputText)
+{
+    let newText = "";
+    if (inputText)
+    {
+        for (let i=0;i<inputText.length;i++)
+        {
+            newText += i==0 ? inputText[i].toUpperCase() : inputText[i];
+        }
+    }
+    return newText;
+}
